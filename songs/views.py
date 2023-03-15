26ad1f5366062c3008313d53c7bd8c2410ce4864
Ali Mogharrab <alimogharrab101@gmail.com>
2023-03-15 17:25:14 +0330

    Add index page

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Index(View):
+    def get(self, request):
+        context = {}
+        return render(request, 'songs/index.html', context=context)
