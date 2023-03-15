678a54630547169e5fc7f595765ee832442c0b15
Ali Mogharrab <alimogharrab101@gmail.com>
2023-03-15 22:51:11 +0330

    Add Profile model

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,23 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    GENDER_CHOICES = (
+        ('m', 'Male'),
+        ('f', 'Female'),
+        ('o', 'Other'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    @property
+    def is_artist(self):
+        return hasattr(self, 'artist')
