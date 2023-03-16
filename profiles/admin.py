7b2e823523be68639bd152c0be662bd58a05022c
Ali Mogharrab <alimogharrab101@gmail.com>
2023-03-16 23:02:17 +0330

    Register Profile model in admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'age', 'gender', 'created', 'is_artist']
+    list_editable = ['is_artist']
