64f691bfd4e409cbf4fcedaa3d8e4dd0640b7cc7
Ali Mogharrab <alimogharrab101@gmail.com>
2023-03-16 19:43:56 +0330

    Add RegisterUser view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+class RegisterUser(View):
+    def get(self, request):
+        form = CustomUserCreationForm()
+        context = {'form': form}
+        return render(request, 'profiles/register.html', context=context)
+
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created successfully')
+
+        else:
+            messages.error(request, 'An error occurred during registration')
+
+        return redirect('index')
+    
