33ce258e831f0faac7f8bef91aef72f59f56daf3
Ali Mogharrab <alimogharrab101@gmail.com>
2023-03-17 17:42:18 +0330

    Add Artist model

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,14 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from profiles.models import Profile
+
+
+class Artist(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    nickname = models.CharField(max_length=50, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
