e9e570feaeaa68076262203c806f6dbd1bd25910
Ali Mogharrab <alimogharrab101@gmail.com>
2023-04-07 20:05:58 +0330

    Add ReviewForm

--- songs/forms.py
+++ songs/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Album, Song
+from .models import Album, Song, Review
 
 
 class SongForm(forms.ModelForm):
@@ -30,6 +30,23 @@ class AlbumForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(AlbumForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+
+        labels = {
+            'value': 'Place your vote',
+            'body': 'Add a comment with your vote',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
