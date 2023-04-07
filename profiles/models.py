d113e531000ce2f9f6694c9d83718062ca75a23a
Ali Mogharrab <alimogharrab101@gmail.com>
2023-04-07 19:49:45 +0330

    Add Review model

--- profiles/models.py
+++ profiles/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    
+
     def __str__(self):
         return str(self.user.username)
 
--- songs/models.py
+++ songs/models.py
@@ -36,9 +36,51 @@ class Song(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
     image = models.ImageField(null=True, blank=True, upload_to='song_covers', default='defaults/default_song.jpg')
     song_file = models.FileField(null=True, blank=True, upload_to='song_files')
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
+    class Meta:
+        ordering = ['-vote_ratio', '-vote_total', 'name']
+
+    @property
+    def reviewers(self):
+        query_set = self.review_set.all().values_list('owner__id', flat=True)
+        return query_set
+
+    @property
+    def set_vote_count(self):
+        reviews = self.review_set.all()
+        up_votes = reviews.filter(value='up').count()
+        total_votes = reviews.count()
+
+        ratio = (up_votes / total_votes) * 100
+
+        self.vote_total = total_votes
+        self.vote_ratio = ratio
+        self.save()
+
     def __str__(self):
         return str(self.name)
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up  vote'),
+        ('down', 'Down vote'),
+    )
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    value = models.CharField(max_length=10, choices=VOTE_TYPE)
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    class Meta:
+        unique_together = [['owner', 'song']]
+
+    def __str__(self):
+        return self.value
